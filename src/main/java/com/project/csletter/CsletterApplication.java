34dc46fe6d20711e1957443ecc69c4ecc66dcab8
Lee-DoHa <johnny550@naver.com>
2022-11-04 16:51:30 +0900

    project update

---
+++ src/main/java/com/project/csletter/CsletterApplication.java
@@ -0,0 +1,13 @@
+package com.project.csletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/csletter/CsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.csletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
