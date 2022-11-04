a33ef8b51ea6934e7769c499c08aa78b11e86a29
Lee-DoHa <johnny550@naver.com>
2022-11-05 00:49:55 +0900

    Update MemberController.java

--- src/main/java/com/project/csletter/member/controller/MemberController.java
+++ src/main/java/com/project/csletter/member/controller/MemberController.java
@@ -5,7 +5,6 @@ import com.project.csletter.member.domain.Member;
 import com.project.csletter.member.domain.OAuthToken;
 import com.project.csletter.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
