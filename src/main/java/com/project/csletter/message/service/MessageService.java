37433b6d8189872a721e94da1d90196748b5eb00
Lee-DoHa <johnny550@naver.com>
2022-11-06 23:57:37 +0900

    Create MessageService.java

---
+++ src/main/java/com/project/csletter/message/service/MessageService.java
@@ -0,0 +1,24 @@
+package com.project.csletter.message.service;
+
+import com.project.csletter.message.domain.Message;
+import com.project.csletter.message.domain.MessageCreate;
+import com.project.csletter.message.repository.MessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public void write(MessageCreate messageCreate) {
+        Message message = Message.builder()
+                .body(messageCreate.getBody())
+                .build();
+
+        messageRepository.save(message);
+    }
+
+
+}
