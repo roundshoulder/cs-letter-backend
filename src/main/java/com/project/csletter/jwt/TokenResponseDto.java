8cb5f6a29ec8120e61dfcb565932e56fb6f166a6
Lee-DoHa <johnny550@naver.com>
2022-11-08 17:10:38 +0900

    Create TokenResponseDto.java

---
+++ src/main/java/com/project/csletter/jwt/TokenResponseDto.java
@@ -0,0 +1,13 @@
+package com.project.csletter.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponseDto {
+    String accessToken;
+    String refreshToken;
+}
