927e9ccaaeea21e11b12279db212ba25feae1f9e
Lee-DoHa <johnny550@naver.com>
2022-11-08 17:58:55 +0900

    Update MessageCreate.java

--- src/main/java/com/project/csletter/message/domain/MessageCreate.java
+++ src/main/java/com/project/csletter/message/domain/MessageCreate.java
@@ -9,13 +9,15 @@ import lombok.*;
 public class MessageCreate {
 
     private String body;
-
     private String nickname;
+    private Long toUserId;
+
 
     @Builder
-    public MessageCreate(String body, String nickname) {
+    public MessageCreate(String body, String nickname, Long toUserId) {
         this.body = body;
         this.nickname = nickname;
+        this.toUserId = toUserId;
     }
 
 }
