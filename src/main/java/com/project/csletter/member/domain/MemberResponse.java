d9dcc2895cab3369bed6dc96ff58ea7fdd1be1a8
Lee-DoHa <johnny550@naver.com>
2022-11-08 17:10:32 +0900

    Create MemberResponse.java

---
+++ src/main/java/com/project/csletter/member/domain/MemberResponse.java
@@ -0,0 +1,27 @@
+package com.project.csletter.member.domain;
+
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Builder
+public class MemberResponse {
+
+    private Long userCode;
+
+    private Long kakaoId;
+
+    private String kakaoProfileImg;
+
+    private String kakaoNickname;
+
+    private String kakaoEmail;
+
+    private String userRole;
+
+    private String refreshToken;
+}
