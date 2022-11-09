5f82d1c98daaae9cb6e633f49339884207dd2682
Lee-DoHa <johnny550@naver.com>
2022-11-09 14:04:23 +0900

    Update CorsConfig.java

--- src/main/java/com/project/csletter/global/config/CorsConfig.java
+++ src/main/java/com/project/csletter/global/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         config.addAllowedHeader("*");
         config.addExposedHeader("*");
         config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
