ddac65884bf1aedc659cbeda5da2a4bf2f3407ab
Lee-DoHa <johnny550@naver.com>
2022-11-09 13:25:52 +0900

    Update MessageResponse.java

--- src/main/java/com/project/csletter/message/domain/MessageResponse.java
+++ src/main/java/com/project/csletter/message/domain/MessageResponse.java
@@ -8,14 +8,14 @@ public class MessageResponse {
     private Long messageId;
     private String body;
     private String nickname;
-    private Long toUserId;
+    private Long toUserCode;
 
     @Builder
     public MessageResponse(Message message) {
         this.messageId = message.getId();
         this.body = message.getBody();
         this.nickname = message.getNickname();
-        this.toUserId = message.getToUserId();
+        this.toUserCode = message.getToUserCode();
     }
 
 }
