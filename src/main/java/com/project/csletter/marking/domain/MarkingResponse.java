355c44fcceab353e8fd7cdd4be6e2ef33da0a749
Lee-DoHa <johnny550@naver.com>
2022-11-11 01:12:37 +0900

    Create MarkingResponse.java

---
+++ src/main/java/com/project/csletter/marking/domain/MarkingResponse.java
@@ -0,0 +1,14 @@
+package com.project.csletter.marking.domain;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Builder
+public class MarkingResponse {
+    private Boolean[] result;
+    private Long totalCount;
+}
