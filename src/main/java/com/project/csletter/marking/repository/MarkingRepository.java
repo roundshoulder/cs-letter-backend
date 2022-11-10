96584011dd6b00904d157049b78e06737b0703d4
Lee-DoHa <johnny550@naver.com>
2022-11-11 01:12:42 +0900

    Update MarkingRepository.java

--- src/main/java/com/project/csletter/marking/repository/MarkingRepository.java
+++ src/main/java/com/project/csletter/marking/repository/MarkingRepository.java
@@ -1,4 +1,11 @@
 package com.project.csletter.marking.repository;
 
-public interface MarkingRepository {
+import com.project.csletter.marking.domain.Marking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MarkingRepository extends JpaRepository<Marking, Long> {
+
+    Optional<Marking> findByMessageId(long messageId);
 }
