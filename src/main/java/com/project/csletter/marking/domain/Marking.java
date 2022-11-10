d316a40fd894a619e38f4b87f82c796ee30efa52
Lee-DoHa <johnny550@naver.com>
2022-11-10 22:05:08 +0900

    Update Marking.java

--- src/main/java/com/project/csletter/marking/domain/Marking.java
+++ src/main/java/com/project/csletter/marking/domain/Marking.java
@@ -1,4 +1,22 @@
 package com.project.csletter.marking.domain;
 
-public class Marking {
+import com.project.csletter.global.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+public class Marking extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "marking_id")
+    private Long id;
+
+    
 }
