c9306426e8f33500c19a61838b6b43070f487e0d
Lee-DoHa <johnny550@naver.com>
2022-11-11 02:39:35 +0900

    Update MarkingController.java

--- src/main/java/com/project/csletter/marking/controller/MarkingController.java
+++ src/main/java/com/project/csletter/marking/controller/MarkingController.java
@@ -1,22 +1,23 @@
 package com.project.csletter.marking.controller;
 
+import com.project.csletter.marking.domain.Marking;
 import com.project.csletter.marking.domain.MarkingCreate;
+import com.project.csletter.marking.domain.MarkingLastResponse;
 import com.project.csletter.marking.domain.MarkingResponse;
+import com.project.csletter.marking.repository.MarkingRepository;
 import com.project.csletter.marking.service.MarkingService;
 import com.project.csletter.message.domain.Message;
 import com.project.csletter.message.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class MarkingController {
 
     private final MarkingService markingService;
+    private final MarkingRepository markingRepository;
     private final MessageRepository messageRepository;
 
     @PostMapping("/marking")
@@ -30,5 +31,12 @@ public class MarkingController {
         Boolean[] result = new Boolean[message.getBody().length()];;
 
         return markingService.getResult(markingCreate, message, result);
+    }
+
+    @GetMapping("/marking/{messageId}")
+    public MarkingLastResponse getMarking(@PathVariable Long messageId) {
+        Marking marking = markingRepository.findByMessageId(messageId).orElseThrow();
+        return MarkingLastResponse.builder()
+                .body(marking.getBody()).build();
     }
 }
