20684cba9a0d15bd8dc1848533d3e5dd7fcdcc0d
Lee-DoHa <johnny550@naver.com>
2022-11-11 01:12:35 +0900

    Update Marking.java

--- src/main/java/com/project/csletter/marking/domain/Marking.java
+++ src/main/java/com/project/csletter/marking/domain/Marking.java
@@ -2,6 +2,7 @@ package com.project.csletter.marking.domain;
 
 import com.project.csletter.global.BaseTimeEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @AllArgsConstructor
+@Builder
 public class Marking extends BaseTimeEntity {
 
     @Id
@@ -18,5 +20,25 @@ public class Marking extends BaseTimeEntity {
     @Column(name = "marking_id")
     private Long id;
 
-    
+    private String body;
+
+    private Long count;
+
+    private Long totalCount;
+
+    private Long messageId;
+
+    public MarkingUpdater.MarkingUpdaterBuilder toUpdater() {
+        return MarkingUpdater.builder()
+                .body(body)
+                .count(count)
+                .totalCount(totalCount);
+
+    }
+
+    public void update(MarkingUpdater markingUpdater) {
+        body = markingUpdater.getBody();
+        count = markingUpdater.getCount();
+        totalCount = markingUpdater.getTotalCount();
+    }
 }
