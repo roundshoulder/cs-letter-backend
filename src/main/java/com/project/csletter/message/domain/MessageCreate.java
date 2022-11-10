1d8f380719b49c75078a6e4321a18cb7df775383
Lee-DoHa <johnny550@naver.com>
2022-11-10 18:42:10 +0900

    Update MessageCreate.java

--- src/main/java/com/project/csletter/message/domain/MessageCreate.java
+++ src/main/java/com/project/csletter/message/domain/MessageCreate.java
@@ -11,13 +11,15 @@ public class MessageCreate {
     private String body;
     private String nickname;
     private String toMemberToken;
+    private Long color;
 
 
     @Builder
-    public MessageCreate(String body, String nickname, String toMemberToken) {
+    public MessageCreate(String body, String nickname, String toMemberToken, Long color) {
         this.body = body;
         this.nickname = nickname;
         this.toMemberToken = toMemberToken;
+        this.color = color;
     }
 
 }
