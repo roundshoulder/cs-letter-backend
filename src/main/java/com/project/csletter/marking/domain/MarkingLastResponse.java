eab5464302ce91d0d2a9de73ea8a7447445cdbd6
Lee-DoHa <johnny550@naver.com>
2022-11-11 02:39:33 +0900

    Create MarkingLastResponse.java

---
+++ src/main/java/com/project/csletter/marking/domain/MarkingLastResponse.java
@@ -0,0 +1,13 @@
+package com.project.csletter.marking.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Builder
+public class MarkingLastResponse {
+
+    private String body;
+}
