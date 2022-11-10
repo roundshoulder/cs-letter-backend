c2c1d622ddfe2c1ee03ea6533702f356e967e39a
Lee-DoHa <johnny550@naver.com>
2022-11-11 01:53:09 +0900

    Update CsletterApplication.java

--- src/main/java/com/project/csletter/CsletterApplication.java
+++ src/main/java/com/project/csletter/CsletterApplication.java
@@ -2,9 +2,21 @@ package com.project.csletter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaAuditing
 public class CsletterApplication {
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsletterApplication.class, args);
