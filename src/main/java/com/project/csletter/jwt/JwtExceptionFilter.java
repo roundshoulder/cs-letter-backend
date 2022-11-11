b1b83291fa6f432fd32f901856586d7944b39639
Lee-DoHa <johnny550@naver.com>
2022-11-12 00:53:06 +0900

    Update JwtExceptionFilter.java

--- src/main/java/com/project/csletter/jwt/JwtExceptionFilter.java
+++ src/main/java/com/project/csletter/jwt/JwtExceptionFilter.java
@@ -14,9 +14,15 @@ import java.io.IOException;
 public class JwtExceptionFilter extends OncePerRequestFilter {
 
     ObjectMapper objectMapper;
+    private final String NO_CHECK_URL = "/oauth/token";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+            filterChain.doFilter(request, response);
+            return; // 이거 안해주면 아래로 내려가서 계속 필터를 진행해버림
+        }
 
         try {
             filterChain.doFilter(request, response);
