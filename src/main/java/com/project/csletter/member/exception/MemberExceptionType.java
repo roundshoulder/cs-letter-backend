bec0f7ab1acc3059f129a57eca10fb809d767387
Lee-DoHa <johnny550@naver.com>
2022-11-11 23:30:38 +0900

    Update MemberExceptionType.java

--- src/main/java/com/project/csletter/member/exception/MemberExceptionType.java
+++ src/main/java/com/project/csletter/member/exception/MemberExceptionType.java
@@ -7,7 +7,7 @@ public enum MemberExceptionType implements BaseExceptionType {
     ALREADY_EXIST_USERNAME(600, HttpStatus.CONFLICT, "이미 존재하는 아이디입니다."),
     WRONG_PASSWORD(601, HttpStatus.BAD_REQUEST, "비밀번호가 잘못되었습니다."),
     NOT_FOUND_MEMBER(602, HttpStatus.NOT_FOUND, "회원 정보가 없습니다."),
-    TOKEN_INVALID(401, HttpStatus.BAD_REQUEST, "token expired");
+    TOKEN_INVALID(401, HttpStatus.BAD_REQUEST, "토큰이 만료되었습니다.");
 
     private int errorCode;
     private HttpStatus httpStatus;
