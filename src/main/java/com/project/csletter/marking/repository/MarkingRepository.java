5173faf1004256917732c02d5a406c6b8f79b416
Lee-DoHa <johnny550@naver.com>
2022-11-14 17:32:37 +0900

    fix query

--- src/main/java/com/project/csletter/marking/repository/MarkingRepository.java
+++ src/main/java/com/project/csletter/marking/repository/MarkingRepository.java
@@ -11,6 +11,6 @@ public interface MarkingRepository extends JpaRepository<Marking, Long> {
 
     Optional<Marking> findByMessageId(long messageId);
 
-    @Query(value = "SELECT * FROM marking", nativeQuery = true)
+    @Query(value = "SELECT * FROM csletter_rds.marking", nativeQuery = true)
     List<Marking> getList();
 }
