8ba0a3c985d31042393b802dd9a7aac3866c6fa5
Lee-DoHa <johnny550@naver.com>
2022-11-20 20:12:52 +0900

    Update MarkingUpdater.java

--- src/main/java/com/project/csletter/marking/domain/MarkingUpdater.java
+++ src/main/java/com/project/csletter/marking/domain/MarkingUpdater.java
@@ -3,15 +3,17 @@ package com.project.csletter.marking.domain;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class MarkingUpdater {
 
-    private String body;
+    private List<String> body;
     private Long count;
     private Long totalCount;
 
     @Builder
-    public MarkingUpdater(String body, Long count, Long totalCount) {
+    public MarkingUpdater(List<String> body, Long count, Long totalCount) {
         this.body = body;
         this.count = count;
         this.totalCount = totalCount;
