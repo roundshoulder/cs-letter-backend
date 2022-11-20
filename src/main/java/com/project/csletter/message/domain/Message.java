c9522e92b41839d5c245bf2b4b935d45c62cf24b
Lee-DoHa <johnny550@naver.com>
2022-11-20 20:12:45 +0900

    Update Message.java

--- src/main/java/com/project/csletter/message/domain/Message.java
+++ src/main/java/com/project/csletter/message/domain/Message.java
@@ -2,13 +2,17 @@ package com.project.csletter.message.domain;
 
 import com.project.csletter.global.BaseTimeEntity;
 import lombok.*;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @AllArgsConstructor
+@Builder
 public class Message extends BaseTimeEntity {
 
     @Id
@@ -16,22 +20,16 @@ public class Message extends BaseTimeEntity {
     @Column(name = "message_id")
     private Long id;
 
-    @Column(nullable = false)
-    private String body;
+    @Column(name = "body")
+    @ElementCollection(targetClass = String.class)
+    @Builder.Default
+    private List<String> body = new ArrayList<>();
 
     private String nickname;
 
     private String toMemberToken;
 
     private Long color;
-
-    @Builder
-    public Message(String body, String nickname, String toMemberToken, Long color) {
-        this.body = body;
-        this.nickname = nickname;
-        this.toMemberToken = toMemberToken;
-        this.color = color;
-    }
 
 
 }
