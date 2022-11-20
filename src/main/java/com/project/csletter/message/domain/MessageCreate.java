c9d7008910025584f15c1b85df3445ca23e41765
Lee-DoHa <johnny550@naver.com>
2022-11-20 20:12:44 +0900

    Update MessageCreate.java

--- src/main/java/com/project/csletter/message/domain/MessageCreate.java
+++ src/main/java/com/project/csletter/message/domain/MessageCreate.java
@@ -2,20 +2,22 @@ package com.project.csletter.message.domain;
 
 import lombok.*;
 
+import java.util.List;
+
 @Data
 @Getter
 @Setter
 @ToString
 public class MessageCreate {
 
-    private String body;
+    private List<String> body;
     private String nickname;
     private String toMemberToken;
     private Long color;
 
 
     @Builder
-    public MessageCreate(String body, String nickname, String toMemberToken, Long color) {
+    public MessageCreate(List<String> body, String nickname, String toMemberToken, Long color) {
         this.body = body;
         this.nickname = nickname;
         this.toMemberToken = toMemberToken;
