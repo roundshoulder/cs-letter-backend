97826785c42ae9f7b6f0e58f8d62c1644d4651d3
Lee-DoHa <johnny550@naver.com>
2022-11-24 22:14:10 +0900

    줄바꿈 수정

--- src/main/java/com/project/csletter/message/service/MessageService.java
+++ src/main/java/com/project/csletter/message/service/MessageService.java
@@ -47,6 +47,18 @@ public class MessageService {
                 }
                 tmpString = messageCreate.getBody().substring(startIndex, startIndex+23);
                 int lastIndex = tmpString.length()-1;
+
+                if(tmpString.contains("\n")) {
+                    for(int j = 0; j < tmpString.length(); j++) {
+                        if(tmpString.charAt(j) == '\n') {
+                            lastIndex = j;
+                            tmpString = tmpString.substring(startIndex, j+1);
+                            i--;
+                            break;
+                        }
+                    }
+                }
+
                 if(tmpString.contains(" ")) {
                     while(tmpString.charAt(lastIndex) != ' ') {
                         lastIndex--;
