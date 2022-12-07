39029f27580a91c9d7a0198196a39131408e4712
Lee-DoHa <johnny550@naver.com>
2022-12-07 13:19:06 +0900

    Create MemberUpdate.java

---
+++ src/main/java/com/project/csletter/member/domain/MemberUpdate.java
@@ -0,0 +1,12 @@
+package com.project.csletter.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberUpdate {
+    String nickname;
+}
